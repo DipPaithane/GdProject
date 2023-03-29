8d90e2160fda72e8a10ca807a5d6b5f02f52f621
DeepaPaithane <deepalipaithane86@gmail.com>
2023-03-29 15:59:38 +0530

    First commit

---
+++ src/main/java/com/practice/MinMax.java
@@ -0,0 +1,29 @@
+package com.practice;
+
+public class MinMax {
+	public static void main(String[] args) {
+
+		int a[] = { 5, 2, 30, 10, 6 };
+		int max = 30;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > max) {
+				max = a[i];
+			}
+		}
+		System.out.println("max no." + max);
+		System.out.println("------------------------------------------------------------------------------");
+		int min = 10;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] < min) {
+				min = a[i];
+			}
+		}
+		System.out.println("min no." + min);
+		System.out.println("--------------------------------------------------------------------------------");
+
+		System.out.println("difference between" + (max - min));
+		System.out.println("---------------------------------------------------------------------------------");
+
+	}
+
+}
